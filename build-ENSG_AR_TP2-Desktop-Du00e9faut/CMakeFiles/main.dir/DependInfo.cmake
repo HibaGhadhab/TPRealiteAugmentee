8fd13854ecfb3a6ec9595eb461ce3ad39d47987c
HibaGhadhab <hibaghadhab@ensg.eu>
2019-01-25 20:16:26 +0100

    TP réalité augmentée commit 1

---
+++ build-ENSG_AR_TP2-Desktop-Du00e9faut/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/formation/Documents/RA/ENSG_AR_TP2/main.cpp" "/home/formation/Documents/RA/build-ENSG_AR_TP2-Desktop-Du00e9faut/CMakeFiles/main.dir/main.cpp.o"
+  "/home/formation/Documents/RA/ENSG_AR_TP2/opengl_code/Shader.cpp" "/home/formation/Documents/RA/build-ENSG_AR_TP2-Desktop-Du00e9faut/CMakeFiles/main.dir/opengl_code/Shader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/formation/Documents/RA/ENSG_AR_TP2"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/include/eigen3"
+  "/home/formation/Documents/RA/ORB_SLAM2/include"
+  "/home/formation/Documents/RA/ORB_SLAM2"
+  "/home/formation/Documents/RA/ENSG_AR_TP2/opengl_code"
+  "/usr/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-ORB_SLAM2-Desktop-Du00e9faut/CMakeFiles/orbslam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbslam.dir/Examples/orbslam.cpp.o"
+  "/home/formation/Documents/RA/ORB_SLAM2/Examples/orbslam.pdb"
+  "/home/formation/Documents/RA/ORB_SLAM2/Examples/orbslam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
